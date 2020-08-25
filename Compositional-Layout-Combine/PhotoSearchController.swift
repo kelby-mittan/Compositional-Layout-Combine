e29ee0620dfc27b3ee9fb63845e92de48fe0eeb0
Kelby Mittan <kelbymittan@pursuit.org>
2020-08-25 13:21:12 -0400

    update

--- Compositional-Layout-Combine/PhotoSearchController.swift
+++ Compositional-Layout-Combine/PhotoSearchController.swift
@@ -7,126 +7,149 @@
 //
 
 import UIKit
-import Combine
+import Combine // asynchronous programming framework introduced in iOS 13
+import Kingfisher
 
 class PhotoSearchController: UIViewController {
+  
+  enum SectionKind: Int, CaseIterable {
+    case main
+  }
+  
+  // declare collection view
+  private var collectionView: UICollectionView!
+  
+  // declare data source
+  typealias DataSource = UICollectionViewDiffableDataSource<SectionKind, Photo>
+  private var dataSource: DataSource!
+  
+  // declare a search controller
+  private var searchController: UISearchController!
+  
+  // declare a serchText property that will be a `Publisher`
+  // that emits changes from the searchBar on the search controller
+  // in order to make any property a `Publisher` you need to append
+  // the `@Published` property wrapper
+  // to subscribe to the searchText's `Publisher` a $ needs to be prefixed
+  // to searchText => $searchText
+  @Published private var searchText = ""
+  
+  // store subscriptions
+  private var subscriptions: Set<AnyCancellable> = []
+  
+  // property observer
+//  var searchText {
+//    didSet {
+//      // changes
+//    }
+//  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.title = "Photo Search"
+    configureCollectionView()
+    configureDataSource()
+    configureSearchController()
     
-    enum SectionKind: Int, CaseIterable {
-        case main
+    // subscribe to the searchText `Publisher`
+    $searchText
+      .debounce(for: .seconds(1.0), scheduler: RunLoop.main)
+      .removeDuplicates()
+      .sink { [weak self] (text) in // .assign
+        self?.searchPhotos(for: text)
+        // call the api client for the photo search queue
+      }
+      .store(in: &subscriptions)
+  }
+  
+  private func searchPhotos(for query: String) {
+    // searchPhotos is a `Publisher`
+    APIClient().searchPhotos(for: query)
+      .sink(receiveCompletion: { (completion) in
+        print(completion)
+      }) { [weak self] (photos) in
+        self?.updateSnapshot(with: photos)
+      }
+      .store(in: &subscriptions)
+  }
+  
+  private func updateSnapshot(with photos: [Photo]) {
+    var snapshot = dataSource.snapshot()
+    snapshot.deleteAllItems()
+    snapshot.appendSections([.main])
+    snapshot.appendItems(photos)
+    dataSource.apply(snapshot, animatingDifferences: false)
+  }
+  
+  private func configureSearchController() {
+    searchController = UISearchController(searchResultsController: nil)
+    navigationItem.searchController = searchController
+    searchController.searchResultsUpdater = self // delegate
+    searchController.searchBar.autocapitalizationType = .none
+    searchController.obscuresBackgroundDuringPresentation = false
+  }
+  
+  private func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+    collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.reuseIdentifier)
+    collectionView.backgroundColor = .systemBackground
+    collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    view.addSubview(collectionView)
+  }
+  
+  private func createLayout() -> UICollectionViewLayout {
+    let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+      // item
+      let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+      let item = NSCollectionLayoutItem(layoutSize: itemSize)
+      let itemSpacing: CGFloat = 5 // points
+      item.contentInsets = NSDirectionalEdgeInsets(top: itemSpacing, leading: itemSpacing, bottom: itemSpacing, trailing: itemSpacing)
+      
+      // group (leadingGroup, trailingGroup, nestedGroup)
+      let innerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.50), heightDimension: .fractionalHeight(1.0))
+      let leadingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 2)
+      let trailingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 3)
+      let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(1000))
+      let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [leadingGroup, trailingGroup])
+      
+      // section
+      let section = NSCollectionLayoutSection(group: nestedGroup)
+      
+      return section
     }
     
-    private var collectionView: UICollectionView!
+    // layout
+    return layout
+  }
+  
+  private func configureDataSource() {
+    // initializing the data source and
+    // configuring the cell
+    dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, photo) -> UICollectionViewCell? in
+      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCell.reuseIdentifier, for: indexPath) as? ImageCell else {
+        fatalError("could not dequeue an ImageCell")
+      }
+      cell.imageView.kf.indicatorType = .activity
+      cell.imageView.kf.setImage(with: URL(string: photo.webformatURL))
+      cell.imageView.contentMode = .scaleAspectFill
+      return cell
+    })
     
-    typealias DataSource = UICollectionViewDiffableDataSource<SectionKind,Int>
-    
-    private var dataSource: DataSource!
-    
-    private var searchController: UISearchController!
-    
-    
-    @Published private var searchText = ""
-    
-    // store subscriptions
-    private var subsriptions: Set<AnyCancellable> = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationItem.title = "Photo Search"
-        view.backgroundColor = .systemBackground
-        
-        configureCollectionView()
-        configureDataSource()
-        configSearchController()
-        
-        $searchText
-            .debounce(for: .seconds(1.0), scheduler: RunLoop.main)
-            .removeDuplicates()
-            .sink { (text) in
-                print(text)
-                // call the api
-                self.searchPhotos(for: text)
-        }
-        .store(in: &subsriptions)
-    }
-    
-    private func searchPhotos(for query: String) {
-        APIClient().searchPhotos(for: query)
-            .sink(receiveCompletion: { (completion) in
-                print(completion)
-            }) { (photos) in
-                dump(photos)
-        }
-        .store(in: &subsriptions)
-    }
-    
-    private func configSearchController() {
-        searchController = UISearchController(searchResultsController: nil)
-        navigationItem.searchController = searchController
-        searchController.searchResultsUpdater = self // delegate
-        searchController.searchBar.autocapitalizationType = .none
-        searchController.obscuresBackgroundDuringPresentation = false
-    }
-    
-    private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
-        collectionView.backgroundColor = .systemBackground
-        collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.reuseIdentifier)
-        collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
-        view.addSubview(collectionView)
-    }
-    
-    
-    private func createLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
-            
-            // item
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            let itemSpacing: CGFloat = 5
-            item.contentInsets = NSDirectionalEdgeInsets(top: itemSpacing, leading: itemSpacing, bottom: itemSpacing, trailing: itemSpacing)
-            
-            // group (leadingGroup, trailingGroup, nestedGroup)
-            let innerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
-            let leadingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 2)
-            let trailingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 3)
-            let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(1000))
-            let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [leadingGroup,trailingGroup])
-            
-            // section
-            let section = NSCollectionLayoutSection(group: nestedGroup)
-            return section
-        }
-        
-        return layout
-    }
-    
-    private func configureDataSource() {
-        dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell? in
-            
-            // configure cell and return cell
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCell.reuseIdentifier, for: indexPath) as? ImageCell else {
-                fatalError("could not dequeue")
-            }
-            cell.backgroundColor = .systemTeal
-            cell.layer.cornerRadius = 12
-            return cell
-        })
-        
-        
-        var snapshot = dataSource.snapshot()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(Array(1...7))
-        dataSource.apply(snapshot, animatingDifferences: false)
-    }
+    // setup initial snapshot
+    var snapshot = dataSource.snapshot() // current snapshot
+    snapshot.appendSections([.main])
+    //snapshot.appendItems(Array(1...4))
+    dataSource.apply(snapshot, animatingDifferences: false)
+  }
 }
 
 extension PhotoSearchController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text, !text.isEmpty else {
-            return
-        }
-        searchText = text
+  func updateSearchResults(for searchController: UISearchController) {
+    guard let text = searchController.searchBar.text,
+      !text.isEmpty else {
+        return
     }
-    
+    searchText = text
+    // upon asigning a new value to the searchText
+    // the subscriber in the viewDidLoad will receive that value
+  }
 }
